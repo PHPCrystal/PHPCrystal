6106be9e78e103a89e388996b1f48dcf209ea1c6
Andrei Samuilik <a.samuylik@gmail.com>
2015-08-04 11:35:58 +0300

    Package installation testing

--- src/Service/Event/Context/Cli.php
+++ src/Service/Event/Context/Cli.php
@@ -3,8 +3,19 @@ namespace PHPCrystal\PHPCrystal\Service\Event\Context;
 
 class Cli extends AbstractContext
 {
+	/**
+	 * @return string
+	 */
 	public function getEnv()
 	{
 		return $this->get('env');
+	}
+	
+	/**
+	 * @return string
+	 */
+	public function getHostname()
+	{
+		return 'localhost';
 	}
 }
